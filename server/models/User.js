58e4e0ef3f5b6f37df0415f3b9992de24215cbd5
leancaiguara <leancaiguara@gmail.com>
2021-12-23 16:22:15 -0300

    model-user

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const User = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: false }
+);
+
+module.exports = model("User", User);
